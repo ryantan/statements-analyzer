98e3ddc3df3ef4d11f654290b0334cee208470a4
Ryan Tan <ryan@redairship.com>
2025-07-26 22:11:38 +0800

    chore: set up antd for next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+// To make antd v5 support react 19
+
 import './globals.css';
 
 const geistSans = Geist({
@@ -28,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
   );
