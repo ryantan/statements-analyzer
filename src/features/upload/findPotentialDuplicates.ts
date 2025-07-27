0c547fe41a243ef73696b43d9263967fc878c277
Ryan Tan <ryan@redairship.com>
2025-07-27 15:45:02 +0800

    feat(upload): implement helpers to detect duplication

---
+++ src/features/upload/findPotentialDuplicates.ts
@@ -0,0 +1,26 @@
+import { Transaction } from '@/features/transactions/types';
+
+import groupBy from 'lodash/groupBy';
+import uniqBy from 'lodash/unionBy';
+
+export const findPotentialDuplicates = (transactions: Transaction[]) => {
+  const uniqueFunction = (item: Transaction) =>
+    `${item.dateFormatted}|${item.amountFormatted}|${item.description.join(' ')}`;
+
+  const duplicateCheck = uniqBy(transactions, uniqueFunction);
+  console.log('Items in allTransactions:', transactions.length);
+  console.log('Items in duplicateCheck:', duplicateCheck.length);
+
+  const potentialDuplicates = transactions.length - duplicateCheck.length;
+  if (potentialDuplicates > 0) {
+    console.log(`${potentialDuplicates} potential duplicates found.`);
+
+    const duplicates = groupBy(transactions, uniqueFunction);
+    for (const duplicateGroup of Object.values(duplicates)) {
+      if (duplicateGroup.length === 1) {
+        continue;
+      }
+      console.log('Duplicate group:', duplicateGroup);
+    }
+  }
+};
